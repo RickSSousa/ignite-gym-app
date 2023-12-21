2ca585881d9567e6b036826fcfaed3f42ab19c61
Ricardo S. Sousa <ricksantos.rs25@gmail.com>
2023-12-21 16:11:36 -0300

    Refactor History, Home and Profile screens.

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,46 @@
-import { Center, Text } from "native-base";
+import { HistoryCard } from "@components/HistoryCard";
+import { ScreenHeader } from "@components/ScreenHeader";
+import { Heading, SectionList, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function History() {
+
+  const [exercises, setExercises] = useState([
+    {
+      title: "20.12.23",
+      data: ["Puxada frontal", "Puxada baixa"]
+    },
+    {
+      title: "21.12.23",
+      data: ["Puxada baixa"]
+    }
+  ]);
+
   return (
-    <Center flex={1}>
-      <Text color={'white'}>History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeader title="Histórico de exercícios" />
+
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) =>
+          <HistoryCard />
+        }
+        renderSectionHeader={({ section }) => (
+          <Heading color={'gray.200'} fontSize={'md'} mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        ListEmptyComponent={() => (
+          <Text color={'gray.100'} textAlign={'center'}>
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercícios hoje?
+          </Text>
+        )}
+        contentContainerStyle={exercises.length === 0 && { flex: 1, justifyContent: 'center' }}
+        showsVerticalScrollIndicator={false}
+        px={8}
+      />
+    </VStack>
   );
 }
--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,57 @@
-import { Center, Text } from "native-base";
+import { ExerciseCard } from "@components/ExerciseCard";
+import { Group } from "@components/Group";
+import { HomeHeader } from "@components/HomeHeader";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
+import { useState } from "react";
 
 export function Home() {
+
+  const [groups, setGroups] = useState(['costas', 'bíceps', 'peito', 'pernas', 'ombros', 'tríceps', 'abdominais', 'antebraços', 'trapézio', 'glúteos', 'panturrilhas', 'cardio']);
+  const [exercises, setExercises] = useState(['Remada baixa', 'Puxada alta', 'Puxada frontal', 'Puxada supinada', 'Remada cavalinho', 'Remada curvada', 'Remada unilateral', 'Remada vertical']);
+  const [groupSelected, setGroupSelected] = useState('costas');
+
   return (
-    <Center flex={1}>
-      <Text color={'white'}>Home</Text>
-    </Center>
+    <VStack flex={1}>
+      <HomeHeader />
+
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <Group
+            name={item}
+            isActive={groupSelected.toLocaleUpperCase() === item.toLocaleUpperCase()}
+            onPress={() => setGroupSelected(item)}
+          />
+        )}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={'space-between'} mb={5}>
+          <Heading color={'gray.200'} fontSize={'md'}>
+            Exercícios
+          </Heading>
+          <Text color={'gray.200'} fontSize={'sm'}>
+            {exercises.length}
+          </Text>
+        </HStack>
+
+        <FlatList
+          data={exercises}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => (
+            <ExerciseCard />
+          )}
+          _contentContainerStyle={{ paddingBottom: 20 }}
+          showsVerticalScrollIndicator={false}
+        />
+      </VStack>
+
+    </VStack>
   );
 }
