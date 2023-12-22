b713a7f409148327a8d21e172e7c2d40e55bb33e
Ricardo S. Sousa <ricksantos.rs25@gmail.com>
2023-12-22 11:26:18 -0300

    Update ExerciseCard, HistoryCard, Home, and Exercise components

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,12 +3,20 @@ import { Group } from "@components/Group";
 import { HomeHeader } from "@components/HomeHeader";
 import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 import { useState } from "react";
+import { useNavigation } from '@react-navigation/native';
+import { AppNavigatorRoutesProps } from "@routes/app.routes";
 
 export function Home() {
 
   const [groups, setGroups] = useState(['costas', 'bíceps', 'peito', 'pernas', 'ombros', 'tríceps', 'abdominais', 'antebraços', 'trapézio', 'glúteos', 'panturrilhas', 'cardio']);
   const [exercises, setExercises] = useState(['Remada baixa', 'Puxada alta', 'Puxada frontal', 'Puxada supinada', 'Remada cavalinho', 'Remada curvada', 'Remada unilateral', 'Remada vertical']);
   const [groupSelected, setGroupSelected] = useState('costas');
+
+  const navigation = useNavigation<AppNavigatorRoutesProps>();
+
+  const handleOpenExerciseDetails = () => {
+    navigation.navigate('exercise');
+  };
 
   return (
     <VStack flex={1}>
@@ -29,6 +37,7 @@ export function Home() {
         _contentContainerStyle={{ px: 8 }}
         my={10}
         maxH={10}
+        minH={10}
       />
 
       <VStack flex={1} px={8}>
@@ -45,7 +54,9 @@ export function Home() {
           data={exercises}
           keyExtractor={(item) => item}
           renderItem={({ item }) => (
-            <ExerciseCard />
+            <ExerciseCard
+              onPress={handleOpenExerciseDetails}
+            />
           )}
           _contentContainerStyle={{ paddingBottom: 20 }}
           showsVerticalScrollIndicator={false}
