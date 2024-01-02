0f021fcc1963583cecb88b85559e88d597c48e83
Ricardo S. Sousa <ricksantos.rs25@gmail.com>
2024-01-02 16:31:00 -0300

    Update font family in components and add hookform and validations

--- src/screens/SignUp.tsx
+++ src/screens/SignUp.tsx
@@ -4,13 +4,38 @@ import LogoSvg from "@assets/logo.svg";
 import { Input } from "@components/Input";
 import { Button } from "@components/Button";
 import { useNavigation } from '@react-navigation/native';
+import { useForm, Controller } from "react-hook-form";
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
+
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+};
+
+const signUpSchema = yup.object().shape({
+  name: yup.string().required('Nome obrigatório.'),
+  email: yup.string().required('E-mail obrigatório.').email('E-mail inválido.'),
+  password: yup.string().required('Senha obrigatória.').min(6, 'A senha deve ter no mínimo 6 caracteres.'),
+  confirmPassword: yup.string().required('Confirme a senha.').oneOf([yup.ref('password')], 'As senhas devem ser iguais.'),
+});
 
 export function SignUp() {
+
+  const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
+    resolver: yupResolver(signUpSchema)
+  });
 
   const navigation = useNavigation();
 
   const handleGoBack = () => {
     navigation.goBack();
+  };
+
+  const handleSignUp = (data: FormDataProps) => {
+
   };
 
   return (
@@ -36,30 +61,74 @@ export function SignUp() {
             Crie sua conta
           </Heading>
 
-          <Input
-            placeholder="E-mail"
-            keyboardType="email-address"
-            autoCapitalize="none"
-          />
-          <Input
-            placeholder="Nome"
-          />
-          <Input
-            placeholder="Senha"
-            secureTextEntry
-          />
-          <Input
-            placeholder="Confirme a senha"
-            secureTextEntry
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.name?.message}
+              />
+            )}
           />
 
-          <Button title="Criar e acessar" />
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="E-mail"
+                keyboardType="email-address"
+                autoCapitalize="none"
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.email?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="password"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                errorMessage={errors.password?.message}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="confirmPassword"
+            render={({ field: { onChange, value } }) => (
+              <Input
+                placeholder="Confirme a senha"
+                secureTextEntry
+                onChangeText={onChange}
+                value={value}
+                onSubmitEditing={handleSubmit(handleSignUp)}
+                returnKeyType="send"
+                errorMessage={errors.confirmPassword?.message}
+              />
+            )}
+          />
+
+          <Button
+            title="Criar e acessar"
+            onPress={handleSubmit(handleSignUp)}
+          />
         </Center>
 
         <Button
           title="Voltar para o login"
           variant='outline'
-          mt={24}
+          mt={12}
           onPress={handleGoBack}
         />
       </VStack>
